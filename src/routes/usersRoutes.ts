04bb55297c28fcef1fe6da4ac4ce07011dbc8053
silvestr3 <fellipe.silvestre@outlook.com>
2024-07-26 14:45:54 -0300

    feat: Implement user metrics endpoint

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { knex } from "../database";
 import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
+import { checkUserIdExists } from "../middlewares/check-cookie-exists";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get("/", async () => {
@@ -36,4 +37,37 @@ export async function usersRoutes(app: FastifyInstance) {
 
     return reply.status(201).send();
   });
+
+  app.get(
+    "/metrics",
+    { preHandler: checkUserIdExists },
+    async (request, reply) => {
+      const user_id = request.cookies.user_id;
+
+      const meals = await knex("meals").where("user_id", user_id).select("*");
+
+      const totalMeals = meals.length;
+      const onDietMeals = meals.reduce((total, meal) => {
+        return (total += meal.isOnDiet ? 1 : 0);
+      }, 0);
+      const offDietMeals = totalMeals - onDietMeals;
+
+      let bestStreak = 0;
+      let currentStreak = 0;
+
+      for (let index = 0; index < totalMeals; index++) {
+        currentStreak += meals[index].isOnDiet ? 1 : 0;
+
+        if (!meals[index].isOnDiet || meals[index + 1] === undefined) {
+          if (currentStreak > bestStreak) {
+            bestStreak = currentStreak;
+          }
+
+          currentStreak = 0;
+        }
+      }
+
+      return { totalMeals, onDietMeals, offDietMeals, bestStreak };
+    }
+  );
 }
