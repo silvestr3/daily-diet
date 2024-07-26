d58197de709d39cdcf675a2c68bcac3eb1cecad6
silvestr3 <fellipe.silvestre@outlook.com>
2024-07-26 08:33:15 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello world";
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server is running");
+  });
